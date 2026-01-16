c43db47f7e6c9a8dd24aae9b624e72d01bba3eaf
ashmdavis <68405151+ashmdavis@users.noreply.github.com>
2026-01-15 20:37:47 -0500

    numbers appended to display

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const num1 = "";
 const num2 = "";
 const operator = "";
 
+const display = document.querySelector(".display");
+
 
 function add(num1, num2) {
     console.log(num1 + num2);
@@ -37,11 +39,34 @@ function logicButton() {
     const buttons = document.querySelectorAll(".button");
     buttons.forEach((button) => {
         button.addEventListener("click", (event) => {
-            let target = event.target.id;
-            console.log(target);
+            let target = event.target;
+            let keyValue = target.textContent;
+            if (target.classList.contains("btn-clear")) {
+                clearDisplay();
+            } else if (target.classList.contains("btn-back")) {
+                display.value = display.value.slice(0, -1);
+                // button opperators and decimal can only be displayed once
+            } else if (target.classList.contains("btn-operator") || target.classList.contains("btn-decimal")) {
+                if (display.value != "" && !display.value.includes(keyValue)) {
+                    appendToDisplay(keyValue);
+
+                }
+            }
+            else {
+                appendToDisplay(keyValue);
+            }
         });
-    });  
+    });
 }
 logicButton();
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
 
 
